9c30414fd1c885a187eb191a1ef6a6320cb6900c
Paulina Sadowska <PaulinaSadowska@users.noreply.github.com>
2022-12-04 21:59:17 +0100

    split classes

--- app/src/main/java/com/paulina/sadowska/connectdots/PathsController.kt
+++ app/src/main/java/com/paulina/sadowska/connectdots/PathsController.kt
@@ -53,33 +53,3 @@ data class Path(
         val color: Color,
         val finished: Boolean
 )
-
-class CirclesController(
-        numOfElements: Int,
-        boardSize: Float,
-        private val radiusPx: Int,
-        private val colors: List<Color>
-) {
-    private val multiplier = 1.0f / (numOfElements + 1)
-    val circles = (1..numOfElements).flatMap { i ->
-        val offsetX = boardSize * i * multiplier
-        (1..numOfElements).map { j ->
-            val offsetY = boardSize * j * multiplier
-            Circle(Offset(offsetX, offsetY), colors.random())
-        }
-    }
-
-    fun findCircleInPoint(point: Offset): Pair<Circle, Int>? {
-        val circle = circles.find { circle ->
-            val circlePosition = circle.position
-            point.x > (circlePosition.x - radiusPx) && point.x < (circlePosition.x + radiusPx) &&
-                    point.y > (circlePosition.y - radiusPx) && point.y < (circlePosition.y + radiusPx)
-        }
-        return circle?.let { Pair(circle, circles.indexOf(circle)) }
-    }
-}
-
-data class Circle(
-        val position: Offset,
-        val color: Color
-)
